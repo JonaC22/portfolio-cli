8ffa9ef2dc7a98800f314d6fb07767db6558b039
Jonathan <JonaC22@users.noreply.github.com>
2021-02-13 17:01:53 -0300

    chore: refactor coingecko fetch

--- src/lib/coingecko.rs
+++ src/lib/coingecko.rs
@@ -2,29 +2,16 @@ use serde_json::{Error, Value};
 use std::thread::sleep;
 use std::time::Duration;
 
-pub async fn get_token_id_from_contract_address(contract_address: &str, verbose: bool) -> String {
-    let url = format!(
-        "https://api.coingecko.com/api/v3/coins/ethereum/contract/{}",
-        contract_address
-    );
+pub async fn fetch(url: &String, verbose: bool) -> Value {
     let mut retry: u32 = 0;
     let max_retries: u32 = 5;
 
     loop {
-        let body = reqwest::get(&url).await.unwrap().text().await.unwrap();
+        let body = reqwest::get(url).await.unwrap().text().await.unwrap();
         let result: Result<Value, Error> = serde_json::from_str(&body);
 
         match result {
-            Ok(json) => {
-                let mix_selector = Some(r#""id""#);
-
-                let results = jql::walker(&json, mix_selector).unwrap_or_default();
-
-                match results {
-                    Value::String(value) => return value.parse::<String>().unwrap(),
-                    _ => return "".to_string(),
-                }
-            }
+            Ok(json) => return json,
             _ => {
                 if retry > max_retries {
                     panic!("Could not fetch from coingecko: response body: {:?}", &body);
@@ -43,44 +30,38 @@ pub async fn get_token_id_from_contract_address(contract_address: &str, verbose:
     }
 }
 
+pub async fn get_token_id_from_contract_address(contract_address: &str, verbose: bool) -> String {
+    let url = format!(
+        "https://api.coingecko.com/api/v3/coins/ethereum/contract/{}",
+        contract_address
+    );
+    let json = fetch(&url, verbose).await;
+
+    let mix_selector = Some(r#""id""#);
+
+    let results = jql::walker(&json, mix_selector).unwrap_or_default();
+
+    match results {
+        Value::String(value) => return value.parse::<String>().unwrap(),
+        _ => return "".to_string(),
+    }
+}
+
 pub async fn get_token_price(token_id: &str, versus_name: &str, verbose: bool) -> f64 {
     let url = format!(
         "https://api.coingecko.com/api/v3/simple/price?ids={}&vs_currencies={}",
         token_id, versus_name
     );
-    let mut retry: u32 = 0;
-    let max_retries: u32 = 5;
+    let json = fetch(&url, verbose).await;
 
-    loop {
-        let body = reqwest::get(&url).await.unwrap().text().await.unwrap();
-        let result: Result<Value, Error> = serde_json::from_str(&body);
-        match result {
-            Ok(json) => {
-                let selector = format!(r#""{}"."{}""#, token_id, versus_name);
-                let mix_selector = Some(selector.as_str());
+    let selector = format!(r#""{}"."{}""#, token_id, versus_name);
+    let mix_selector = Some(selector.as_str());
 
-                let results = jql::walker(&json, mix_selector).unwrap_or_default();
+    let results = jql::walker(&json, mix_selector).unwrap_or_default();
 
-                match results {
-                    Value::Number(value) => return value.to_string().parse::<f64>().unwrap(),
-                    _ => return 0.0,
-                }
-            }
-            _ => {
-                if retry > max_retries {
-                    panic!("Could not fetch from coingecko: response body: {:?}", &body);
-                } else {
-                    retry += 1;
-                    if verbose {
-                        println!(
-                            "Failed to fetch from coingecko, retry up to {}, retry number: {}",
-                            max_retries, retry
-                        );
-                    }
-                    sleep(Duration::from_millis((2_u32.pow(retry) * 1000).into()));
-                }
-            }
-        }
+    match results {
+        Value::Number(value) => return value.to_string().parse::<f64>().unwrap(),
+        _ => return 0.0,
     }
 }
 
