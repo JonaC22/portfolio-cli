127900065764ef02d539cb6a54cfa3289a111989
Jonathan <JonaC22@users.noreply.github.com>
2021-05-22 23:21:19 -0300

    chore: use shorter implementations

--- src/lib/paraswap.rs
+++ src/lib/paraswap.rs
@@ -69,9 +69,9 @@ pub async fn get_token_price(
 
     Ok(value
         .as_str()
-        .unwrap_or_else(|| "0.0")
+        .unwrap_or("0.0")
         .parse::<f64>()
-        .unwrap_or_else(|_e| 0.0))
+        .unwrap_or(0.0))
 }
 
 #[cfg(test)]
